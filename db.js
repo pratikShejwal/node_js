2fefa571c209955e54f8b198d0f388772620de5f
pratikShejwal <shejwalp147@gmail.com>
2025-06-21 22:09:00 +0530

    hosting database

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 // Define MongoDB URL
 
-const mongoURL = 'mongodb://127.0.0.1:27017/resto'
+// const mongoURL = 'mongodb://127.0.0.1:27017/resto'
+const mongoURL = 'mongodb+srv://pkshejwal009:NaVf7iZFDobxjS2t@cluster0.csm9jhl.mongodb.net/'
 
 //set up db connection
 mongoose.connect(mongoURL,{
