5665b0cae9810b1325d404cee31eef9c88308db7
pratikShejwal <shejwalp147@gmail.com>
2025-07-02 16:16:36 +0530

    jwt token

---
+++ jwt.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const jwtAuthMiddleware = (req,res,next)=>{
+    const token = req.headers.authorization.split(' ')[1]
+    if(!token) return res.status(401).json({error:"unauthorized"})
+
+    try {
+      const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY)
+    
+      //pass user information to req object
+      req.user = decoded
+      next()
+    } 
+    catch (error) 
+    {
+        console.error(error);    
+        res.status(401).json({error:"unauthorized"})
+    }
+}
+
+
+const generateToken = (userData) =>{
+    return jwt.sign(userData,process.env.JWT_SECRET_KEY)
+}
+module.exports = jwtAuthMiddleware
